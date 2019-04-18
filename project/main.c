f806648a9b77c3307f0afeaea7d97c45525e3306
Kevin Sears <Kevin.w.sears@nasa.gov>
2019-04-18 13:37:43 -0400

    added main.c

--- project/main.c
+++ project/main.c
@@ -4,8 +4,17 @@
 int main() 
 {
 
-    printf("Version 0.0.0\n");
-    printf("$Id$ $Format:%ci$ ($Format:%h$)\n");
-    
+    printf("Version 0.0.1\n");
+    printf("$Id: 06e014f50a35c06eee7738fd019283910edfe7a0 $ $build_date$ $poop$\n");
+    // $Id: 06e014f50a35c06eee7738fd019283910edfe7a0 $ 
+    // $Format:%ci$ ($Format:%h$)
+    // WHYYYY
+    // $ZYX$
+   // lol idk man lslsls
+    //// test add commit
+
     return 1;
 }
+
+
+// https://github.com/gilesbowkett/git-smudge-and-clean
