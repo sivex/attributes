70bf5375530daf4611bca50c6b0034db24e66b23
Kevin Sears <Kevin.w.sears@nasa.gov>
2019-04-18 13:56:48 -0400

    test again

--- project/main.c
+++ project/main.c
@@ -5,13 +5,13 @@ int main()
 {
 
     printf("Version 0.0.1\n");
-    printf("$Id: 06e014f50a35c06eee7738fd019283910edfe7a0 $ $build_date$ $poop$\n");
-    // $Id: 06e014f50a35c06eee7738fd019283910edfe7a0 $ 
+    printf("$Id$ $build_date$ $poop$\n");
+    // $Id$ 
     // $Format:%ci$ ($Format:%h$)
     // WHYYYY
     // $ZYX$
    // lol idk man lslsls
-    //// test add commit
+    //// test add commit   hahahah 
 
     return 1;
 }
